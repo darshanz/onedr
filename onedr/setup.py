fd51c0b19eb7dc3c60e8219c2266cf80707d6240
sudarshan <sudarshan.pant@gmail.com>
2025-05-07 18:26:55 +0100

    started onedr

---
+++ onedr/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='onedr',
+    version='0.1.0',
+    description='onedr is a python library for drone control using MAVLink.',
+    url='https://github.com/darshanz/onedr',
+    author='Sudarshan Pant',
+    author_email='sudarshan.pant@.ucd.ie',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    install_requires=['pymavlink'],
+
+)
